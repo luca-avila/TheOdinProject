258762ec7170dfa90b37f84b9871e4f797c2fca8
luca-avila <avilaluca61@gmail.com>
2025-07-31 15:39:18 -0300

    Complete js-exercises 07

--- foundations/javascript-exercises/07_reverseString/reverseString.js
+++ foundations/javascript-exercises/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const len = str.length;
+    let reversed = '';
 
+    for (let i = len -1; i >= 0; i--){
+        reversed += str[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
