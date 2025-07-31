1561bd5334ab663a397c1947a5cf3db0139b2ce6
luca-avila <avilaluca61@gmail.com>
2025-07-31 15:33:39 -0300

    Complete js-exercises 06

--- foundations/javascript-exercises/06_repeatString/repeatString.js
+++ foundations/javascript-exercises/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
 
+    if (times < 0) return 'ERROR';
+
+    let repeated = '';
+
+    for (let i = 0; i < times; i++){
+        repeated += str;
+    }
+    return repeated;
 };
 
 // Do not edit below this line
