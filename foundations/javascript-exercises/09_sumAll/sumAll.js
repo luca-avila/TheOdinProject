358f711462421e9c34d53818bb86cc6f002d1339
luca-avila <avilaluca61@gmail.com>
2025-07-31 16:19:15 -0300

    Complete js-exercises 09

--- foundations/javascript-exercises/09_sumAll/sumAll.js
+++ foundations/javascript-exercises/09_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let major = 0;
+    let minor = 0;
 
+    if (a < 0 ||
+         b < 0 ||
+        typeof a != 'number'||
+        typeof b != 'number' ||
+        Math.floor(a) != a ||
+        Math.floor(b) != b
+    ) return 'ERROR';
+
+    if (b > a) {
+        major = b;
+        minor =  a;
+    } else {
+        major = a;
+        minor = b;
+    }
+
+
+    let sum = 0;
+    for (let i = minor; i <= major; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
