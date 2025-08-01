ca3b9b3c99953c7723e31765ad398aa4218ee737
luca-avila <avilaluca61@gmail.com>
2025-08-01 19:36:04 -0300

    Add style

--- foundations/Etch-a-sketch/logic.js
+++ foundations/Etch-a-sketch/logic.js
@@ -2,7 +2,7 @@ export function setClickEvent(container) {
     const squares = container.querySelectorAll('.square');
     squares.forEach(square => {
         square.addEventListener('mouseover', ()=>{
-            square.style.backgroundColor = 'black';
+            square.style.backgroundColor = '#183642';
         });
     });
 }
