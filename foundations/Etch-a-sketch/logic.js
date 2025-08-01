627f12c9c5c62e8760a8f5da1c5848843b6c48b4
luca-avila <avilaluca61@gmail.com>
2025-08-01 19:27:42 -0300

    Add validation to grid side length

--- foundations/Etch-a-sketch/logic.js
+++ foundations/Etch-a-sketch/logic.js
@@ -11,6 +11,15 @@ export function setStartButtonEvent(form, callback){
     const startButton = form.querySelector('button');
     startButton.addEventListener('click', ()=>{
         const sideLength = form.querySelector('input').value;
+
+        if (sideLength < 1 || sideLength > 100) {
+            alert('Please enter a number between 1 and 100');
+            return;
+        } else if (isNaN(sideLength)) {
+            alert('Please enter a valid number');
+            return;
+        }
+
         callback(sideLength);
     });
 }
