fed3fd1165aab44373d2ca1b373da22179b69ca5
luca-avila <avilaluca61@gmail.com>
2025-08-01 18:49:38 -0300

    Implement createGrid and setSquareSize functions)

---
+++ foundations/Etch-a-sketch/ui.js
@@ -0,0 +1,19 @@
+function createGrid(gridSize) {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+
+    for (let i = 0; i < gridSize * gridSize; i++){
+        const square = document.createElement('div');
+        square.classList.add('square');
+        grid.appendChild(square);
+    }
+    return grid;
+}
+
+function setSquareSize(container, gridSize, squareAmount, className) {
+    const squares = container.querySelectorAll(`.${className}`);
+    squares.forEach(square => {
+        square.style.width = `calc(${gridSize}px / ${squareAmount})`;
+        square.style.height = `calc(${gridSize}px / ${squareAmount})`;
+    });
+}
