21636bc29a4e99ccfd3b2f16da8e6e10ddba6263
luca-avila <avilaluca61@gmail.com>
2025-08-04 15:22:16 -0300

    Create buttons on main.js

--- projects/Calculator/main.js
+++ projects/Calculator/main.js
@@ -1,4 +1,10 @@
-// Create calculator and add to the container
+import { createAddButton, createSubstractButton, createMultiplyButton, createDivideButton, createEqualButton, createNumberButton, createClearButton, createDeleteButton } from './ui.js';
 
-// Select screen
-
+const calculatorContainer = document.getElementById('calculator');
+let buttonsContainer = document.getElementById('buttons');
+let screenContainer = document.getElementById('screen');
+let operand1 = document.getElementById('operand1');
+let operand2 = document.getElementById('operand2');
+let operator = document.getElementById('operator');
+let result = document.getElementById('result');
+let buttons = [];
--- projects/Calculator/ui.js
+++ projects/Calculator/ui.js
@@ -1,46 +1,46 @@
-function createAddButton() {
+export function createAddButton() {
     const button = document.createElement('div');
     button.classList.add('addButton');
     button.textContent = '+';
 }
 
-function createSubstractButton() {
+export function createSubstractButton() {
     const button = document.createElement('div');
     button.classList.add('substractButton');
     button.textContent = '-';
 }
 
-function createMultiplyButton() {
+export function createMultiplyButton() {
     const button = document.createElement('div');
     button.classList.add('multiplyButton');
     button.textContent = '*';
 }
 
-function createDivideButton() {
+export function createDivideButton() {
     const button = document.createElement('div');
     button.classList.add('divideButton');
     button.textContent = '/';
 }
 
-function createEqualButton() {
+export function createEqualButton() {
     const button = document.createElement('div');
     button.classList.add('equalButton');
     button.textContent = '=';
 }
 
-function createNumberButton(number) {
+export function createNumberButton(number) {
     const button = document.createElement('div');
     button.classList.add('numberButton');
     button.textContent = number;
 }
 
-function createClearButton() {
+export function createClearButton() {
     const button = document.createElement('div');
     button.classList.add('clearButton');
     button.textContent = 'C';
 }
 
-function createDeleteButton() {
+export function createDeleteButton() {
     const button = document.createElement('div');
     button.classList.add('deleteButton');
     button.textContent = 'DEL';
