9cec734772758e836c2c17904f917cafd79eca2b
luca-avila <avilaluca61@gmail.com>
2025-08-04 13:44:54 -0300

    Complete JS exercises 14

--- foundations/javascript-exercises/14_fibonacci/fibonacci.js
+++ foundations/javascript-exercises/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if (num == 0) return 0;
+    if (num == 1) return 1;
+    if (num < 0) return 'OOPS';
+
+    let prev = 0;
+    let curr = 1;
+    
+    let next = 0;
+
+    for (let i = 2; i <= num; i++) {
+        next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+    return next;
 };
 
 // Do not edit below this line
