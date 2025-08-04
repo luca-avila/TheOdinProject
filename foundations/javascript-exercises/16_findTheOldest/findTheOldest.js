f5d8a3fff323f75e58bcb835cca847c053962b68
luca-avila <avilaluca61@gmail.com>
2025-08-04 14:40:47 -0300

    Complete JS exercises 16

--- foundations/javascript-exercises/16_findTheOldest/findTheOldest.js
+++ foundations/javascript-exercises/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const currentYear = new Date().getFullYear();
+    let oldestAge;
+    let oldestPerson;
 
+    // Initialize with first person
+    oldestAge = arr[0].yearOfDeath ? arr[0].yearOfDeath - arr[0].yearOfBirth : currentYear - arr[0].yearOfBirth;
+    oldestPerson = arr[0];
+    
+    for (let i = 1; i < arr.length; i++){
+        const age = arr[i].yearOfDeath ? arr[i].yearOfDeath - arr[i].yearOfBirth : currentYear - arr[i].yearOfBirth;
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestPerson = arr[i];
+        }
+    }
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
