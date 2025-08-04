946b6531494f53debca76f02ddf4483685ae864f
luca-avila <avilaluca61@gmail.com>
2025-08-04 12:57:27 -0300

    Complete JS exercises 13

--- foundations/javascript-exercises/13_palindromes/palindromes.js
+++ foundations/javascript-exercises/13_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let reversed = '';
+    let onlyLetters = '';
+    const strLen = str.length;
 
+    for (let i = 0; i < strLen; i++) {
+        if (isntLetter(str[i])) continue;
+        onlyLetters += str[i].toLowerCase();
+    }
+
+    const lettersLen = onlyLetters.length;
+
+    for (let i = lettersLen - 1; i >= 0; i--) {
+        reversed += onlyLetters[i];
+    }
+    
+    return reversed === onlyLetters;
 };
 
+function isntLetter(char) {
+    const code = char.charCodeAt(0);
+    if (!(code >= 65 && code <= 90) && !(code >= 97 && code <= 122) && !(code >= 48 && code <= 57)) {
+        return true;
+    }
+    return false;
+}
 // Do not edit below this line
 module.exports = palindromes;
