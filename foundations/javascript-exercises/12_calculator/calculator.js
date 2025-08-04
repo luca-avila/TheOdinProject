d9b620e2b9d964ff84117d2f5ffcde4cdb2754aa
luca-avila <avilaluca61@gmail.com>
2025-08-04 12:39:49 -0300

    Complete JS exercises 12

--- foundations/javascript-exercises/12_calculator/calculator.js
+++ foundations/javascript-exercises/12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(...nums) {
+	  return nums.reduce((accumulator, current) => accumulator + current, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((accumulator, current) => accumulator + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((accumulator, current) => accumulator * current, 1);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let accumulator = num;
+
+  for (let i = 0; i < power - 1; i++) {
+    accumulator = accumulator * num;
+  }
+
+  return accumulator;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let accumulator = 1;
+
+  if (num === 0) return 1;
+
+	for (let i = 1; i <= num; i++) {
+    accumulator *= i;
+  }
+  return accumulator;
 };
 
 // Do not edit below this line
